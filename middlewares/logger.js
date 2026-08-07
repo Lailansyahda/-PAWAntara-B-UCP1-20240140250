599c9a794e6e026f469c0457a7ab754bc9f23a24
Lailansyahda <syahdaazalia123@gmail.com>
2026-08-07 09:30:29 +0700

    Sprint1-tambah middleware pencatat request

---
+++ middlewares/logger.js
@@ -0,0 +1,8 @@
+// middlewares/logger.js
+function requestLogger(req, res, next) {
+  const now = new Date().toISOString();
+  console.log(`[${now}] ${req.method} ${req.originalUrl}`);
+  next();
+}
+
+module.exports = requestLogger;
